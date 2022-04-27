ad5239da3f391888d5cd0e0fe299bef77ba54aad
Lunar Tear <Lunar Tear@LUNARTEAR>
2022-04-27 17:44:17 +1000

    Добавьте файлы проекта.

---
+++ CoffeeSh0p/ItemWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoffeeSh0p
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemWindow.xaml
+    /// </summary>
+    public partial class ItemWindow : Window
+    {
+        public int sum;
+        public ItemWindow(string selectedDrink, string volume, string imageSource)
+        {
+            InitializeComponent();
+            //BtnDown.IsEnabled = false;
+            drink.Text = selectedDrink;
+            vol.Text = volume;
+            picture.Source = new BitmapImage(new Uri(imageSource, UriKind.Relative));
+        }
+
+        private void btn_Num_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_MinNum_Click(object sender, RoutedEventArgs e)
+        {
+            int i = Convert.ToInt32(btn_Num.Content);
+            if (i > 0)
+                btn_Num.Content = i - 1;
+        }
+
+        private void btn_PlusNum_Click(object sender, RoutedEventArgs e)
+        {
+            int i = Convert.ToInt32(btn_Num.Content);
+            if (i < 99)
+                btn_Num.Content = i + 1;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ CoffeeSh0p/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoffeeSh0p
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink="Капучино";
+            string volume = "300 мл";
+            string imageSource = "/Images/cappuccino.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+            
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink = "Капучино XL";
+            string volume = "400 мл";
+            string imageSource = "/Images/cappuccino.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink = "Латте";
+            string volume = "300 мл";
+            string imageSource = "/Images/latte.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink = "Латте XL";
+            string volume = "400 мл";
+            string imageSource = "/Images/latte.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink = "Мокачино";
+            string volume = "300 мл";
+            string imageSource = "/Images/mok.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDrink = "Мокачино XL";
+            string volume = "400 мл";
+            string imageSource = "/Images/mok.png";
+            ItemWindow itemWindow = new ItemWindow(selectedDrink, volume, imageSource);
+            this.Close();
+            itemWindow.Show();
+        }
+    }
+}
